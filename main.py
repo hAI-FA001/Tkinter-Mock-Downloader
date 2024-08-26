e96a5165362424607125078c8f78b1ae1f3d93e6
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-08-26 13:47:28 +0500

    move input below vid label

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ app.geometry("720x480")
 app.title("YouTube Video Downloader")
 
 
-link_var = tkinter.StringVar()
-link_input = customtkinter.CTkEntry(app, width=350, height=40, textvariable=link_var)
-link_input.pack()
 
 vid_label = customtkinter.CTkLabel(app, text="YouTube Link")
 vid_label.pack(padx=10, pady=10)
+
+link_var = tkinter.StringVar()
+link_input = customtkinter.CTkEntry(app, width=350, height=40, textvariable=link_var)
+link_input.pack()
 
 download_btn = customtkinter.CTkButton(app, text="Download")
 download_btn.pack(padx=10, pady=10)
