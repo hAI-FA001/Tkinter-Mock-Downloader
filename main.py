2334ba8c2f60f60f377f970792778bf98ec12dce
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-08-26 13:48:57 +0500

    basic download feature

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import customtkinter
 from pytube import YouTube
 
 
+def downloadVid():
+    try:
+        entered_link = link_input.get()
+        yt_vid = YouTube(entered_link)
+        
+        vid = yt_vid.streams.get_highest_resolution()
+        vid.download()
+        
+    except Exception as e:
+        pass
+
+
 customtkinter.set_appearance_mode('System')  # dark mode/light mode
 customtkinter.set_default_color_theme('blue')
 
@@ -19,7 +31,7 @@ link_var = tkinter.StringVar()
 link_input = customtkinter.CTkEntry(app, width=350, height=40, textvariable=link_var)
 link_input.pack()
 
-download_btn = customtkinter.CTkButton(app, text="Download")
+download_btn = customtkinter.CTkButton(app, text="Download", command=downloadVid)
 download_btn.pack(padx=10, pady=10)
 
 
